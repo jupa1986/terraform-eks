0d724dc4b079435fd68601344db2bf4a1f6c8ee0
jppoma <jppoma@revolutionparts.com>
2024-07-03 11:26:43 -0400

    removing duplicates

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -86,10 +86,6 @@ data "aws_iam_policy" "ebs_csi_policy" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
-
 module "irsa-ebs-csi" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version = "5.39.0"
